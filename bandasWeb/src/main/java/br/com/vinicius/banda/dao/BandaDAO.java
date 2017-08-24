e9febba9b0bd91758d4ca938619f948f1bd470bc
Noturno <Noturno@192.168.7.20>
2017-08-23 21:56:43 -0300

    criação dos métodos DAO

--- bandasWeb/src/main/java/br/com/vinicius/banda/dao/BandaDAO.java
+++ bandasWeb/src/main/java/br/com/vinicius/banda/dao/BandaDAO.java
@@ -1,28 +1,60 @@
 package br.com.vinicius.banda.dao;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import br.com.vinicius.banda.model.Banda;
 
 public class BandaDAO {
 
-	private final Connection con;
+	private final Connection conn;
 
-	public BandaDAO(Connection con) {
-		this.con = con;
+	public BandaDAO(Connection conn) {
+		this.conn = conn;
 	}
 
+	public boolean inserir(Banda banda) throws SQLException {
+		String sql = "INSERT INTO BANDA (BAN_CODIGO, BAN_NOME, BAN_DT_CRIACAO, BAN_PAIS) VALUES (SEQ_BANDA.nextval, ?, ?, ?)";
+
+		PreparedStatement statement = conn.prepareStatement(sql);
+		statement.setString(1, banda.getNome());
+		statement.setDate(2, new Date(banda.getDtCriacao().getTime()));
+		statement.setInt(3, banda.getPais().getCodigo());
+
+		return statement.executeUpdate() > 0;
+	}
+
+	public boolean alterar(Banda banda) throws SQLException {
+		String sql = "UPDATE BANDA SET BAN_NOME = ?, BAN_DT_CRIACAO = ?, BAN_PAIS = ? WHERE BAN_CODIGO = ?";
+
+		PreparedStatement statement = conn.prepareStatement(sql);
+		statement.setString(1, banda.getNome());
+		statement.setDate(2, new Date(banda.getDtCriacao().getTime()));
+		statement.setInt(3, banda.getPais().getCodigo());
+		statement.setInt(4, banda.getCodigo());
+
+		return statement.executeUpdate() > 0;
+	}
+
+	public boolean excluir(Integer codigo) throws SQLException {
+		String sql = "DELETE BANDA WHERE BAN_CODIGO = ?";
+
+		PreparedStatement statement = conn.prepareStatement(sql);
+		statement.setInt(1, codigo);
+
+		return statement.executeUpdate() > 0;
+	}
+	
 	public List<Banda> lista() throws SQLException {
 		List<Banda> bandas = new ArrayList<>();
 
 		String sql = "select * from BANDA";
-		try (PreparedStatement stmt = con.prepareStatement(sql)) {
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 			stmt.execute();
 			try (ResultSet rs = stmt.getResultSet()) {
 				while (rs.next()) {
@@ -34,9 +66,7 @@ public class BandaDAO {
 				}
 			}
 		}
-
 		return bandas;
-
 	}
 	
 }
--- bandasWeb/src/main/java/br/com/vinicius/banda/dao/EstiloDAO.java
+++ bandasWeb/src/main/java/br/com/vinicius/banda/dao/EstiloDAO.java
@@ -11,17 +11,44 @@ import br.com.vinicius.banda.model.Estilo;
 
 public class EstiloDAO {
 
-	private final Connection con;
+	private final Connection conn;
 
 	public EstiloDAO(Connection con) {
-		this.con = con;
+		this.conn = con;
+	}
+	
+	public boolean inserir(Estilo estilo) throws SQLException {
+		String sql = "INSERT INTO ESTILO (EST_CODIGO, EST_NOME) VALUES (SEQ_ESTILO.nextval, ?)";
+
+		PreparedStatement statement = conn.prepareStatement(sql);
+		statement.setString(1, estilo.getNome());
+
+		return statement.executeUpdate() > 0;
+	}
+
+	public boolean alterar(Estilo estilo) throws SQLException {
+		String sql = "UPDATE ESTILO SET EST_NOME = ? WHERE EST_CODIGO = ?";
+
+		PreparedStatement statement = conn.prepareStatement(sql);
+		statement.setString(1, estilo.getNome());
+
+		return statement.executeUpdate() > 0;
+	}
+
+	public boolean excluir(Integer codigo) throws SQLException {
+		String sql = "DELETE ESTILO WHERE EST_CODIGO = ?";
+
+		PreparedStatement statement = conn.prepareStatement(sql);
+		statement.setInt(1, codigo);
+
+		return statement.executeUpdate() > 0;
 	}
 
 	public List<Estilo> lista() throws SQLException {
 		List<Estilo> estilos = new ArrayList<>();
 
 		String sql = "select * from ESTILO";
-		try (PreparedStatement stmt = con.prepareStatement(sql)) {
+		try (PreparedStatement stmt = conn.prepareStatement(sql)) {
 			stmt.execute();
 			try (ResultSet rs = stmt.getResultSet()) {
 				while (rs.next()) {
